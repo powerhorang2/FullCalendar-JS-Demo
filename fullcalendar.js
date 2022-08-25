66d5eac3af0358bfb6fd835dc1a62707f4230968
powerhorang2 <tjrdbqls3@naver.com>
2022-08-25 14:50:18 +0900

    날짜 선택 시 해당 날짜에 active 클래스 추가하는 코드 추가

---
+++ fullcalendar.js
@@ -0,0 +1,26 @@
+let fncCurrentDate = new Date();
+
+function showActiveDate() {
+    let fullYear = fncCurrentDate.getFullYear();
+
+    let month = 0;
+    if (fncCurrentDate.getMonth() < 9) {
+        month = "0" + (fncCurrentDate.getMonth() + 1);
+    } else {
+        month = fncCurrentDate.getMonth() + 1;
+    }
+
+    let date = fncCurrentDate.getDate();
+
+    if (date < 10) {
+        date = "0" + date
+    }
+
+    let currentDateStr = fullYear + "-" + month + "-" + date;
+
+    let currentDateEl = document.querySelector('.fc-daygrid-day[data-date="' + currentDateStr + '"]');
+
+    if (currentDateEl) {
+        currentDateEl.classList.add('active');
+    }
+}
